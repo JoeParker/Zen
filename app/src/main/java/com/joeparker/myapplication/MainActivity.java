7b9f9671ab5e35b792ab6527444946b65898fcf7
Joe Parker <guyman17@yahoo.com>
2019-08-18 16:37:58 +0100

    Append modified files

--- app/src/main/java/com/joeparker/myapplication/MainActivity.java
+++ app/src/main/java/com/joeparker/myapplication/MainActivity.java
@@ -2,13 +2,34 @@ package com.joeparker.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AnimationDrawable androidAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView androidImage = (ImageView)findViewById(R.id.android);
+        androidImage.setBackgroundResource(R.drawable.android_animate);
+        androidAnim = (AnimationDrawable)androidImage.getBackground();
+
+        final Button animationButton = (Button)findViewById(R.id.animate);
+        View.OnClickListener clicked;
+        clicked = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                androidAnim.stop();
+                androidAnim.start();
+            }
+        };
+        animationButton.setOnClickListener(clicked);
     }
 }
