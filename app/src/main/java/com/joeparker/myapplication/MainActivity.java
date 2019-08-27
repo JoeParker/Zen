6c9ad5c5840a9c5215788f738c3ac4c227c23794
Joe Parker <guyman17@yahoo.com>
2019-08-27 01:14:05 +0100

    Add global stop button to action bar

--- app/src/main/java/com/joeparker/myapplication/MainActivity.java
+++ app/src/main/java/com/joeparker/myapplication/MainActivity.java
@@ -3,13 +3,23 @@ package com.joeparker.myapplication;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -114,5 +124,76 @@ public class MainActivity extends AppCompatActivity {
             b.setOnClickListener(listener);
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.save:
+                save(soundMap, this);
+                return true;
+            case R.id.stop:
+                stop(soundMap, this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //Menu button functions
+    private static void save(Map m, Context context) {
+        List<SoundButton> nowPlaying = new ArrayList<>();
+        Collection soundButtons = m.values();
+        for (Object s : soundButtons) {
+            if (s instanceof SoundButton) {
+                SoundButton sb = (SoundButton)s;
+                MediaPlayer player = sb.getPlayer();
+                if (player.isPlaying()) {
+                    nowPlaying.add(sb);
+                }
+            }
+            else {
+                System.err.println("Save error, unable to cast SoundButton object.");
+            }
+        }
+
+        Toast.makeText(context, "Saved selection", Toast.LENGTH_SHORT).show();
+    }
+
+    //Stop all currently playing sounds
+    private static void stop(Map m, Activity context) {
+        List<SoundButton> nowPlaying = new ArrayList<>();
+        Collection soundButtons = m.values();
+        for (Object s : soundButtons) {
+            if (s instanceof SoundButton) {
+                SoundButton sb = (SoundButton)s;
+                MediaPlayer player = sb.getPlayer();
+                if (player.isPlaying()) {
+                    nowPlaying.add(sb);
+                }
+            }
+            else {
+                System.err.println("Stop error, unable to cast SoundButton object.");
+            }
+        }
+        for (SoundButton sb : nowPlaying) {
+            Button button = context.findViewById(R.id.spring_birds);
+            sb.getPlayer().pause();
+            Set<Map.Entry<Button, SoundButton>> entries = m.entrySet();
+            for (Map.Entry entry : entries) {
+                if (Objects.equals(sb, entry.getValue())) {
+                    button = (Button) entry.getKey();
+                }
+            }
+            button.setBackground(sb.getImageOff());
+        }
     }
 }
